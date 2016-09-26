b0215a3cf5009da3ed5f7b5c1a2b2c85bdc80307
lzwjava <lzwjava@gmail.com>
2016-09-27 06:06:44 +0800

    typo

--- src/views/LiveView.vue
+++ src/views/LiveView.vue
@@ -3,7 +3,7 @@
     <div class="player-area" :style="{height: videoHeight + 'px'}">
       <div class="video-wait" v-show="live.status == 10">
         <p class="big-title">离直播开始还有 {{timeGap}}</p>
-        <p class="small-title">感谢参与，开播时您请收到一条短信通知~</p>
+        <p class="small-title">感谢参与，开播时您将收到一条短信通知~</p>
       </div>
       <div class="video-on" v-show="live.status == 20 || live.status == 30">
         <video id="player1" width="100%" height="100%" preload="preload"
