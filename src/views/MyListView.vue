dba6de5d47f425f4fa0011ccd121f118bf44c0c9
lzwjava <lzwjava@gmail.com>
2016-10-05 02:59:32 +0800

    add myListView

---
+++ src/views/MyListView.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div class="my-list-view">
+
+  </div>
+
+</template>
+
+<script type="text/javascript">
+
+export default {
+  name: 'MyListView',
+  components:{
+  },
+  data() {
+    return {
+    }
+  }
+  methods: {
+  }
+}
+
+</script>
+
+<style media="screen" lang="stylus">
+
+</style>
