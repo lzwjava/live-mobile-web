bc1f9edf0fc7ebd3a27f8a8af32f3365fe27b918
lzwjava <lzwjava@gmail.com>
2017-05-06 19:47:42 +0800

    Revert "Yogadev"

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -35,9 +35,9 @@ export default {
     initWechatLogin() {
       var redirectUrl
       if (util.isDebug()) {
-        redirectUrl = 'http://m.quzhiboapp.com/#wechat/webOauthTest'
+        redirectUrl = 'http://quzhiboapp.com/#wechat/webOauthTest'
       } else {
-        redirectUrl = 'http://m.quzhiboapp.com/#wechat/webOauth'
+        redirectUrl = 'http://quzhiboapp.com/#wechat/webOauth'
       }
       var obj = new WxLogin({
         id:'login_container',
