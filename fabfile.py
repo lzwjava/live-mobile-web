b1b2874a7b086977c0ae5535c3887469ad18a6e6
lzwjava <lzwjava@gmail.com>
2018-03-28 19:48:17 +0800

    改进发布脚本

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ from fabric.contrib.project import rsync_project
 from fabric.contrib.files import exists, sed
 from fabric.utils import puts
 
-server_dir = '/home/project/live-mobile-web'
+server_dir = '/home/wwwroot/bbox.tv'
 tmp_dir = '/tmp/live-mobile-web' + str(os.getpid())
 
 def _set_user_dir():
